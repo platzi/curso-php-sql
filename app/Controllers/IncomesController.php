7418b51b0c482afd83c0f2a6a3f55dbf376d2870
RetaxMaster <retaxmaster@hotmail.com>
2022-08-09 13:42:38 -0500

    08 Creacion de controladores

---
+++ app/Controllers/IncomesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+
+class IncomesController {
+
+    /**
+     * Muestra una lista de este recurso
+     */
+    public function index() {}
+
+    /**
+     * Muestra un formulario para crear un nuevo recurso
+     */
+    public function create() {}
+
+    /**
+     * Guarda un nuevo recurso en la base de datos
+     */
+    public function store() {}
+
+    /**
+     * Muestra un único recurso especificado
+     */
+    public function show() {}
+
+    /**
+     * Muestra el formulario para editar un recurso
+     */
+    public function edit() {}
+
+    /**
+     * Actualiza un recurso específico en la base de datos
+     */
+    public function update() {}
+
+    /**
+     * Elimina un recurso específico de la base de datos
+     */
+    public function destroy() {}
+    
+}
+
+/*
+
+index - Display a listing of the resource.
+create - Show the form for creating a new resource.
+store - Store a newly created resource in storage.
+show - Display the specified resource.
+edit - Show the form for editing the specified resource.
+update - Update the specified resource in storage.
+destroy - Remove the specified resource from storage.
+
+*/
---
+++ app/Controllers/WithdrawalsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+class WithdrawalsController {
+
+    /**
+     * Muestra una lista de este recurso
+     */
+    public function index() {}
+
+    /**
+     * Muestra un formulario para crear un nuevo recurso
+     */
+    public function create() {}
+
+    /**
+     * Guarda un nuevo recurso en la base de datos
+     */
+    public function store() {}
+
+    /**
+     * Muestra un único recurso especificado
+     */
+    public function show() {}
+
+    /**
+     * Muestra el formulario para editar un recurso
+     */
+    public function edit() {}
+
+    /**
+     * Actualiza un recurso específico en la base de datos
+     */
+    public function update() {}
+
+    /**
+     * Elimina un recurso específico de la base de datos
+     */
+    public function destroy() {}
+    
+}
