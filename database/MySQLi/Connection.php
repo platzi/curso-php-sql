54224bd7c09e00a86afabbb4e73147a06353eb82
RetaxMaster <retaxmaster@hotmail.com>
2022-08-09 13:22:10 -0500

    07 Refactor de la conexión con clases

--- database/MySQLi/Connection.php
+++ database/MySQLi/Connection.php
@@ -1,26 +1,51 @@
 <?php
 
-$server = "localhost";
-$database = "finanzas_personales";
-$username = "retaxmaster";
-$password = "123";
+namespace Database\MySQLi;
 
-// Esta es la forma procedural
-//$mysqli = mysqli_connect($server, $username, $password, $database);
+class Connection {
 
-// Esta es al forma orientada a objetos
-$mysqli = new mysqli($server, $username, $password, $database);
+    private static $instance;
+    private $connection;
 
-// Comprobar conexión de manera procedural
-/* if (!$mysqli)
-    die("Falló la conexión: " . mysqli_connect_error()); */
+    private function __construct() {
+        $this->make_connection();
+    }
 
-// Comprobar conexión de manera orientada a objetos
-if ($mysqli->connect_errno)
-    die("Falló la conexión: {$mysqli->connect_error}");
+    public static function getInstance() {
 
-// Esto nos ayuda a poder usar cualquier caracter en nuestras consultas
-$setnames = $mysqli->prepare("SET NAMES 'utf8'");
-$setnames->execute();
+        if(!self::$instance instanceof self)
+            self::$instance = new self();
 
-// var_dump($setnames);
+        return self::$instance;
+
+    }
+
+    public function get_database_instance() {
+        return $this->connection;
+    }
+
+    private function make_connection() {
+
+        $server = "localhost";
+        $database = "finanzas_personales";
+        $username = "retaxmaster";
+        $password = "123";
+        
+        // Esta es al forma orientada a objetos
+        $mysqli = new \mysqli($server, $username, $password, $database);
+        
+        // Comprobar conexión de manera orientada a objetos
+        if ($mysqli->connect_errno)
+            die("Falló la conexión: {$mysqli->connect_error}");
+        
+        // Esto nos ayuda a poder usar cualquier caracter en nuestras consultas
+        $setnames = $mysqli->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+
+        $this->connection = $mysqli;
+        
+    }
+    
+}
+
+
