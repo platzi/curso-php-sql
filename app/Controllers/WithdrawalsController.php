3fc2e604ccacb0eb6a28f78245f816c2db8f5c2a
RetaxMaster <retaxmaster@hotmail.com>
2022-08-10 10:41:40 -0500

    12 Consultas PDO

--- app/Controllers/WithdrawalsController.php
+++ app/Controllers/WithdrawalsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Database\PDO\Connection;
+
 class WithdrawalsController {
 
     /**
@@ -17,7 +19,21 @@ class WithdrawalsController {
     /**
      * Guarda un nuevo recurso en la base de datos
      */
-    public function store() {}
+    public function store($data) {
+
+        $connection = Connection::getInstance()->get_database_instance();
+
+        $affected_rows = $connection->exec("INSERT INTO withdrawals (payment_method, type, date, amount, description) VALUES (
+            {$data['payment_method']},
+            {$data['type']},
+            '{$data['date']}',
+            {$data['amount']},
+            '{$data['description']}'
+        )");
+
+        echo "Se han insertado $affected_rows filas en la base de datos.";
+
+    }
 
     /**
      * Muestra un único recurso especificado
